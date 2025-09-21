f8463294b78363945965b7afa0b7fbefa627d6f7
RenanMarinho809 <renanmarinho809@gmail.com>
2025-09-21 15:41:23 -0300

    Setup the project called movie maniac

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./MovieList.css";
+
+const MovieList = () => {
+  return (
+    <section className="movie_list">
+      <header className="align_center movie_list_header"></header>
+    </section>
+  );
+};
+
+export default MovieList;
